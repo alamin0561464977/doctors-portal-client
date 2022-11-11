1bddc2b603d1e459ba15d3989df1bc7d25bb35eb
alamin0561464977 <amin23435678901315@gmail.com>
2022-11-12 02:17:33 +0300

    added a Banner section

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import chair from '../../../assets/images/chair.png';
+import bgChair from '../../../assets/images/bg.png';
+
+const Banner = () => {
+    return (
+        <div className="hero mt-12 bg-base-50">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className=" w-1/2 rounded-lg shadow-2xl" alt='' />
+                <div style={{ backgroundImage: `url("${bgChair}")` }}>
+                    <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
