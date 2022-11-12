88b51edbb52c02c1af96fe5a11fa4e01574ba787
alamin0561464977 <amin23435678901315@gmail.com>
2022-11-12 11:30:01 +0300

    added a Exceptional section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import DoctorsInfoCards from '../DoctorsInfoCards/DoctorsInfoCards';
+import Exceptional from '../Exceptional/Exceptional';
 import OurServices from '../OurServices/OurServices';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <DoctorsInfoCards></DoctorsInfoCards>
             <OurServices></OurServices>
+            <Exceptional></Exceptional>
         </div>
     );
 };
