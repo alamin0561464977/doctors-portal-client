aa40649df564a0d60e0fb7dbfb5a6538cb553e9b
alamin0561464977 <amin23435678901315@gmail.com>
2022-11-14 10:10:26 +0300

    added a Login Form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return (
-        <div>
-            <form className='w-[500px] p-12 rounded-lg mx-auto mt-24 bg-slate-100 shadow-xl'>
-                <h2 className='text-2xl font-bold mb-8 text-center text-primary'>Login</h2>
-                <span className="label-text">Email</span>
-                <input type="email" placeholder="email"
-                    className="input input-bordered font-bold w-full rounded-lg my-3" />
-                <span className="label-text">Password</span>
-                <input type="password" placeholder="pa"
-                    className="input input-bordered font-bold w-full rounded-lg my-3" />
+    const [error, setError] = useState(null);
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
+    const handelLogin = data => {
+        console.log(data)
+    }
+    return (
+        <div className='w-[500px] p-8 rounded-lg mx-auto mt-12 bg-slate-100 shadow-xl'>
+            <form onSubmit={handleSubmit(handelLogin)}>
+                <h2 className='text-4xl font-bold mb-8 text-center text-primary'>Login</h2>
+                {/* email */}
+                <span className="label-text">Email</span>
+                <input type="email" placeholder="email" {...register("email", { required: true })}
+                    className="input input-bordered font-bold w-full rounded-lg my-3" />
+                {errors.email?.type === 'required' && <p role="alert" className=' text-red-600'>Email is required</p>}
+
+                <span className="label-text">Password</span>
+                <input type="password" placeholder="password"  {...register("password", {
+                    required: 'Password is required',
+                    pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8}/, message: 'password must be strong' },
+                })}
+                    className="input input-bordered font-bold w-full rounded-lg my-3" />
+                {errors.password && <p role="alert" className=' text-red-600'>{errors.password.message}</p>}
+                <p>Forgot Forgot <span className=' text-red-300 font-bold'> Password </span> ?</p>
+                {
+                    error &&
+                    <span className="label-text text-red-600 font-bold">auth</span>
+                }
+                <input type="submit"
+                    className="input bg-slate-700 text-white input-border font-bold w-full rounded-lg my-3" />
             </form>
+            <p className='text-lg'>New to Doctors Portal?
+                <Link className='font-bold ml-2 text-green-600' to='/sign-up'>Create new account</Link>
+            </p>
+            <div className="divider">OR</div>
+            <button className="btn text-xl w-full rounded-lg btn-outline">Google Login</button>
         </div>
     );
 };
